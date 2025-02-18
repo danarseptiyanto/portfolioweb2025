3e0189552153c3bb95bbc3299f5e56e9999fe3ff
danarseptiyanto <septiyandanar@gmail.com>
2025-02-19 05:05:20 +0700

    Instal react

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Home');
+});
+
+Route::get('/about', function () {
+    return inertia('About');
 });
